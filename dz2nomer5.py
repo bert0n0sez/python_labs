8be7197554799f2b2a2c4067bf18982d358178e6
Albert <a.iktisanov@yandex.ru>
2023-09-21 21:48:09 +0300

    дз 2 номер 5

---
+++ dz2nomer5.py
@@ -0,0 +1,48 @@
+import turtle
+
+turtle.shape('turtle')
+def krug(x):
+    for i in range(4):
+        turtle.forward(x)
+        turtle.right(90)
+def pere(y):
+    turtle.penup()
+    turtle.goto(-10*y, 10*y)
+    turtle.pendown()
+    krug(x)
+x=10 
+pere(1)
+x +=20 
+pere(2)
+x +=20
+pere(3)
+x +=20
+pere(4)
+x +=20
+pere(5)
+x +=20
+pere(6)
+x +=20
+pere(7)
+x +=20
+pere(8)
+x +=20
+pere(9)
+x +=20
+pere(10)
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
